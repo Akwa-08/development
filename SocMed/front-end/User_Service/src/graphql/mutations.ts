cabb71b9a916936a997f67776603311fa922c9fb
Vinceade Centino <110759413+Akwa-08@users.noreply.github.com>
2025-05-14 11:58:51 +0800

    Add files via upload

---
+++ SocMed/front-end/User_Service/src/graphql/mutations.ts
@@ -0,0 +1,26 @@
+// src/graphql/mutations.ts (or add to queries.ts)
+import { gql } from '@apollo/client';
+
+export const FOLLOW_USER = gql`
+  mutation FollowUser($userIdToFollow: ID!) {
+    followUser(userIdToFollow: $userIdToFollow) {
+      accountId # Return fields needed to update cache, if any
+      isFollowing # Crucially, return the NEW follow status
+    }
+  }
+`;
+
+export const UNFOLLOW_USER = gql`
+  mutation UnfollowUser($userIdToUnfollow: ID!) {
+    unfollowUser(userIdToUnfollow: $userIdToUnfollow) {
+      accountId # Return fields needed to update cache, if any
+      isFollowing # Crucially, return the NEW follow status
+    }
+  }
+`;
+
+export const DELETE_POST = gql`
+  mutation DeletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
